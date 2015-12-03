a121632db1e017ce6180b43226bcde6d13e8df33
SravaniValueLabs <sravanivaluelabs@gmail.com>
2015-12-03 17:41:26 +0530

    Synch

---
+++ scripts/TestDeployBlockly.js
@@ -0,0 +1,8 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block type="scriptr_push_notification" id="16" inline="true" x="1" y="181"><mutation isproduction="false"></mutation><field name="deviceType">android</field></block></xml>*#*#*/
+push(null, null, "android", "null");
+			
---
+++ scripts/script4.js
@@ -0,0 +1,9 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ 
+
+return 0 		   
+	
